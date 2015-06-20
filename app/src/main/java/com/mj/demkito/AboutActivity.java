f67af33b1765b17e7e3d6eaaf0f5bb5a727fcd31
laudix <rehemamagoti@gmail.com>
2015-06-20 14:31:17 +0300

    Pushing....

--- app/src/main/java/com/mj/demkito/AboutActivity.java
+++ app/src/main/java/com/mj/demkito/AboutActivity.java
@@ -2,21 +2,39 @@ package com.mj.demkito;
 
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.about_layout);
 
         ActionBar bar = getSupportActionBar();
         bar.setTitle("About Demkito");
-        bar.setHomeButtonEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(true);
 
+        TextView bodyTv = (TextView) findViewById(R.id.body);
+        String emojis = new String(Character.toChars(0x1f60a));
+        String str = "Caused by: java.lang.NullPointerException\nTry again next time ... "
+                    +"Hah haaha hhaa \nCrazy me!"+emojis;
+        bodyTv.setText(str);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return false;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
